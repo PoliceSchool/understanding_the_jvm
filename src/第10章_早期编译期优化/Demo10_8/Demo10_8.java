b54c017084b517e7c9feaeb752eefeda872c1210
policeschool <policeschool@126.com>
2020-05-13 00:12:57 +0800

    添加第10章代码练习

---
+++ src/第10章_早期编译期优化/Demo10_8/Demo10_8.java
@@ -0,0 +1,19 @@
+package 第10章_早期编译期优化.Demo10_8;
+
+public class Demo10_8 {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+    }
+}
